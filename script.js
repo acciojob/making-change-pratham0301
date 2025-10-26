c390472cbef1e2a99f1734c416119f2517b36845
pratham0301 <70634170+pratham0301@users.noreply.github.com>
2025-10-26 18:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const makeChange = (c) => {
-  // your name here
-};
+    c = parseInt(c);
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+    // Values in cents
+    let q = Math.floor(c / 25);
+    c = c % 25;
+
+    let d = Math.floor(c / 10);
+    c = c % 10;
+
+    let n = Math.floor(c / 5);
+    c = c % 5;
+
+    let p = c;
+
+    return { q, d, n, p };
+};
